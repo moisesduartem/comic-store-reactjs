d88913f10d13600558fc8cfbfdd76db0840aa14a
Moisés Mariano <moises@softbuilder.com.br>
2021-04-06 01:10:56 -0300

    cria header inicial

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { Dropdown, Menu } from 'semantic-ui-react';
+
+function AppHeader() {
+    return (
+        <Fragment>
+            <Menu fixed="top" inverted color="red">
+                <Menu.Item as="a" header position="left">
+                    Comic Store
+                </Menu.Item>
+                <Menu.Item position="right">
+                    <Dropdown text="Login">
+                        <Dropdown.Menu direction="left">
+                            <Dropdown.Item>Acesse com sua conta</Dropdown.Item>
+                            <Dropdown.Item>Não possui conta? <strong>Junte-se a nós!</strong></Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
+            </Menu>
+        </Fragment>
+    );
+}
+
+export default AppHeader;
