6f4e097560af99584e9f7fef37859da1c3f4c80b
Moisés Mariano <moises@softbuilder.com.br>
2021-04-08 00:43:39 -0300

    fix: formata data de modificação

--- src/components/AvailableComicsList/ComicListCard/index.js
+++ src/components/AvailableComicsList/ComicListCard/index.js
@@ -1,12 +1,25 @@
 import React, { Fragment } from 'react';
-import { Card, CardContent, CardDescription, FeedExtra, FeedMeta, Header, Icon, Image } from 'semantic-ui-react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+function getModifiedAt({ date }) {
+    
+    const _date = new Date(date);
+
+    console.log(_date);
+    
+    if (_date == 'Invalid Date') {
+        return '';
+    }
+
+    return `Modificado em ${((_date.getDate())) + "/" + ((_date.getMonth() + 1)) + "/" + _date.getFullYear()}`;
+}
 
 function ComicListCard({ data }) {
 
     const unkownHeroPathImage = '../../../assets/images/unknown_hero.jpg';
-    
+
     const comicPrice = (data?.prices[0]?.price != 0 ? data?.prices[0]?.price : "FREE");
-    
+
     const marvelImagePath = data?.images[0]?.path;
     const cardImage = marvelImagePath ? `${marvelImagePath}/portrait_incredible.jpg` : unkownHeroPathImage;
 
@@ -18,7 +31,7 @@ function ComicListCard({ data }) {
                 <Card.Content>
                     <Card.Header as="a">{data.title}</Card.Header>
                     <Card.Meta>
-                        <span className='date'>{data?.modified}</span>
+                        <span className='date'>{getModifiedAt({ date: data?.modified })}</span>
                     </Card.Meta>
                     <Card.Description>
                         {data?.description}
