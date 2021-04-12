cccbf7438df87fc3a4846902aa646ffb1cd5a67d
Moisés Mariano <moises@softbuilder.com.br>
2021-04-11 22:02:47 -0300

    fix: utiliza os modais do proprio semantic ui

--- src/store/context/index.js
+++ src/store/context/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 import { ThemeProvider } from "styled-components";
-import { ModalProvider } from "./modal";
 
 export function ApplicationContextProvider({ children }) {
     return (
         <ThemeProvider theme={{}}>
-            <ModalProvider>
-                {children}
-            </ModalProvider>
+            {children}
         </ThemeProvider>
     );
 }
